174c864c867ede3ee457c5abb8f44e02a40b0952
oli1230 <obs1230@gmail.com>
2026-05-05 20:17:24 -0500

    adding debug script

---
+++ lasers/debug.lua
@@ -0,0 +1,10 @@
+local chest = peripheral.wrap("bottom")
+local f = fs.open("dump.txt", "w")
+for slot, item in pairs(chest.list()) do
+    if item.name == "buildcraftsilicon:plug_gate" then
+        local meta = chest.getItemMeta(slot)
+        f.writeLine(slot .. ": " .. tostring(meta.displayName) .. " | nbt=" .. tostring(meta.nbtHash) .. " | x" .. item.count)
+    end
+end
+f.close()
+print("Done! Read with: edit dump.txt")
