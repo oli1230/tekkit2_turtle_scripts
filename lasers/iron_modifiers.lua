cab0608d02d60a1ea52bde7cbded69478162d12b
oli1230 <obs1230@gmail.com>
2026-05-05 19:36:47 -0500

    beginning table 2, debugging

--- lasers/iron_modifiers.lua
+++ lasers/iron_modifiers.lua
@@ -3,7 +3,7 @@ local f = fs.open("dump.txt", "w")
 for slot, item in pairs(chest.list()) do
     if item.name == "buildcraftsilicon:plug_gate" then
         local meta = chest.getItemMeta(slot)
-        f.writeLine(slot .. ": " .. tostring(meta.displayName) .. " | nbt=" .. tostring(meta.nbtHash))
+        f.writeLine(slot .. ": " .. tostring(meta.displayName) .. " | nbt=" .. tostring(meta.nbtHash) .. " | x" .. item.count)
     end
 end
 f.close()
